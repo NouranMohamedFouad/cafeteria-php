f1213ae9c5fd3c07b64148adb3a7aa45bafaf59c
NouranMohamedFouad <nouranloly@gmail.com>
2025-03-31 04:25:28 +0200

    add project's structure

---
+++ database/connectionCred.php
@@ -0,0 +1,9 @@
+<?php
+
+const  DB_HOST = "localhost";
+const  DB_USER = "cafeteriauser";
+const  DB_PASSWORD = "cafeteria123456789";
+const  DB_NAME = "cafeteria";
+const DB_PORT = 3306;
+
+?>
---
+++ validations/validate.php
@@ -0,0 +1,31 @@
+<?php
+
+function validatePostData($postData){
+    $errors = [];
+    $valid_data = [];
+    foreach ($postData as $key => $value) {
+        if(! isset($value) or empty($value)){
+            $errors[$key] = ucfirst("{$key} is required");
+        }
+        else if($key == "email" && !validateEmail($value)){
+            $errors["email"] = "Invalid email";
+        }
+        else if($key == "password" && !validatePassword($value)){
+            $errors["password"] = "Invalid Password";
+        }
+        else{
+            $valid_data[$key] = trim($value);
+        }
+    }
+    return ["errors" => $errors, "valid_data" => $valid_data];
+}
+
+function validateEmail($email){
+    return preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email);
+}
+
+function validatePassword($password){
+    return preg_match("/^[a-z0-9_]{8}$/", $password);
+}
+
+?>
