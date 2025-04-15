c296655ad52fce155c683339b4023be26faaebd0
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-04-15 16:09:12 +0200

    feat(product): enhance button styles for edit and delete actions in product management

--- app/products.php
+++ app/products.php
@@ -138,10 +138,10 @@ $products = $productDB->getAllProductsWithCategories();
                                             </span>
                                         </td>
                                         <td>
-                                            <a href="edit_product.php?id=<?= $product['id'] ?>" class="btn btn-sm btn-warning mb-2">
+                                            <a href="edit_product.php?id=<?= $product['id'] ?>" class="btn btn-sm btn-warning mb-2 w-75">
                                                 <i class="fas fa-edit"></i> Edit
                                             </a>
-                                            <button class="btn btn-sm btn-danger delete-btn" data-id="<?= $product['id'] ?>">
+                                            <button class="btn btn-sm btn-danger delete-btn w-75" data-id="<?= $product['id'] ?>">
                                                 <i class="fas fa-trash"></i> Delete
                                             </button>
                                         </td>
